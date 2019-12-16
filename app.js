602b6bf82afba79015337c056dd4b46ee34389b7
unknown <mohamedamdoun95@gmail.com>
2019-12-16 17:38:41 +0100

    broadcast net name

--- app.js
+++ app.js
@@ -22,12 +22,14 @@ const emitPlayers = socket => {
 
 io.on("connection", socket => {
   console.log(`Client ${socket.id} connected`);
-  players.push({id: socket.id, position: {x:50, y:50}})
+  players.push({id: socket.id, name: "", position: {x:50, y:50}})
   setInterval(() => emitPlayers(socket), 1000/60);
   socket.on("position", data => {
     for (var i = 0; i < players.length; i++){
-      if (players[i].id == socket.id)
-        players[i].position = data
+      if (players[i].id == socket.id){
+        players[i].position = data.position;
+        players[i].name = data.name;
+      }
     }
   })
   socket.on("disconnect", () => {
