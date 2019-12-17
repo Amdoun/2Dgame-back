f6519fb43fdf36bb01cc28710cdb6ddfbd149ecc
unknown <mohamedamdoun95@gmail.com>
2019-12-17 22:19:57 +0100

    bullets

--- app.js
+++ app.js
@@ -22,13 +22,14 @@ const emitPlayers = socket => {
 
 io.on("connection", socket => {
   console.log(`Client ${socket.id} connected`);
-  players.push({id: socket.id, name: "", position: {x:50, y:50}})
+  players.push({id: socket.id, name: "", position: {x:50, y:50}, pellets: []})
   setInterval(() => emitPlayers(socket), 1000/60);
   socket.on("position", data => {
     for (var i = 0; i < players.length; i++){
       if (players[i].id == socket.id){
         players[i].position = data.position;
         players[i].name = data.name;
+        players[i].pellets = data.pellets;
       }
     }
   })
